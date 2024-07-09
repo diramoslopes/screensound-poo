88c48089df2a39386f5e14c72a6a0242bc06b339
Diogo <diogo322008@hotmail.com>
2024-07-09 13:26:11 -0300

    criação de atributos e metodo publicos.

--- screensound-poo/Musica.cs
+++ screensound-poo/Musica.cs
@@ -1,7 +1,24 @@
 ﻿class Musica
 {
-    string nome;
-    string artista;
-    int duração;
-    bool disponivel;
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Indisponivel. Adquira outro o plano Plus+");
+        }
+
+    }
+
 }
--- screensound-poo/Program.cs
+++ screensound-poo/Program.cs
@@ -1,4 +1,15 @@
-﻿// nome
-// artista
-// duração
-// disponivel
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 9;
+musica1.disponivel = true;
+
+musica1.ExibirFichaTecnica();
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 12;
+musica2.disponivel = false;
+
+musica2.ExibirFichaTecnica();
