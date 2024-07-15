4621ff40910820471ad6fd83c16812d715b08299
Diogo <diogo322008@hotmail.com>
2024-07-14 22:03:02 -0300

    propriedade (ou atributo) apenas com o metodo get, e com o retorno alterado. Metodo get em formato lambda comentado.

--- screensound-poo/Musica.cs
+++ screensound-poo/Musica.cs
@@ -4,7 +4,15 @@
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-
+    public string DescricaoResumida
+    {
+        get
+        {
+            return $"A música {Nome} pertence à banda {Artista}";
+        }
+    }
+    // quando uma propriedade é somente leitura (get), temos uma outra maneira de escrever o codigo conhecido como lambda, abaixo o exemplo
+    // public String DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"\nNome: {Nome}");
@@ -13,6 +21,7 @@
         if (Disponivel)
         {
             Console.WriteLine("Disponivel no plano.");
+            Console.WriteLine(DescricaoResumida);
         }
         else
         {
